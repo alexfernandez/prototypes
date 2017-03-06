ea4346b2ccf384cd834859668bdb5e90a522b42a
Alex Fernández <alex.fernandez@mediasmart.es>
2017-03-07 00:55:43 +0100

    Escape * as %2A.

--- test/string.js
+++ test/string.js
@@ -131,7 +131,7 @@ function testEscapeAndUnescape(callback)
 {
 	var input = '~!@#$%^&*(){}[]=:/,;?+\'"\\';
 	var escaped = input.escapeForWeb();
-	testing.assertEquals(escaped, '%7E%21@%23%24%25%5E%26*%28%29%7B%7D%5B%5D%3D%3A%2F%2C%3B%3F%2B%27%22%5C', 'Invalid escaped text with symbols', callback);
+	testing.assertEquals(escaped, '%7E%21@%23%24%25%5E%26%2A%28%29%7B%7D%5B%5D%3D%3A%2F%2C%3B%3F%2B%27%22%5C', 'Invalid escaped text with symbols', callback);
 	var firstPass = escaped.unescapeForWeb();
 	testing.assertEquals(firstPass, input, 'Invalid escape + unescape with symbols', callback);
 	testing.assertEquals(input, firstPass.unescapeForWeb(), 'Invalid escape + unescape + unescape with symbols', callback);
